12dbad42391a98c94f50712b8321e7a82f2d883f
Akhila Rao <akhila.rao@sics.se>
2020-07-10 14:25:31 +0200

    Removed an old run script that is not used anymore

--- examples/one_run.sh
+++ examples/one_run.sh
@@ -3,14 +3,14 @@ if [ $# -ne 2 ]; then
        exit 0
 fi
 
-log_loc='/home/akhilarao/data_from_ns3_dash_simulation/dataset7_35Mbps_max_brate_withCa_secondTry'
+log_loc='/home/akhilarao/data_from_ns3_dash_simulation/dataset8_35Mbps_withCa_2sSeg'
 #log_loc='/home/akhilarao/data_from_ns3_dash_simulation/test_3ca'
 mkdir "$log_loc"
 cd ../../../
 pwd
 
-# 8 second segments with 20 ms between frames within the segment 
-framesperseg=400 # 2 second segments
+# 2 second segments with 20 ms between frames within the segment 
+framesperseg=100 # 2 second segments
 sed -i '36s/.*/#define MPEG_FRAMES_PER_SEGMENT '${framesperseg}'/' src/dash/model/mpeg-header.h
 
 # use macroUeDensity=0.000260 for over 250 UEs
@@ -21,12 +21,11 @@ sed -i '36s/.*/#define MPEG_FRAMES_PER_SEGMENT '${framesperseg}'/' src/dash/mode
 # 0.000005417 = 5 UEs
 # 0.000001417 = 1 UEs
 
-len=24
+len=8
 #len=1
 
-for i in 7 8 13 14 15 
-	#16 17 18 19
-#for (( i=0; i<$len; i++ ))
+#for i in 7 8 13 14 15 
+for (( i=0; i<$len; i++ ))
 do
    if [ $i -eq 0 ]; then
       run_type='--run'
@@ -36,20 +35,14 @@ do
       sleep_time=3
    fi
 
-   if [ $i -lt 6 ]; then
-      #macroUeDensity=0.00002875 #  Ues
-      echo $i
+   if [ $i -lt 2 ]; then
       macroUeDensity=0.000020000 # UEs
-   elif [ $i -ge 6 ] && [ $i -lt 12 ]; then
-      echo $i
-      #macroUeDensity=0.00003875 #  Ues
-      macroUeDensity=0.000030000 # 23 UEs
-   elif [ $i -ge 12 ] && [ $i -lt 18 ]; then
-     echo $i 
-     # macroUeDensity=0.00004875 # Ues
-      macroUeDensity=0.000050000 # 23 UEs
+   elif [ $i -ge 2 ] && [ $i -lt 4 ]; then
+      macroUeDensity=0.000030000 # UEs
+   elif [ $i -ge 4 ] && [ $i -lt 6 ]; then
+      macroUeDensity=0.000050000 # UEs
    else
-      macroUeDensity=0.000060000 # 23 UEs
+      macroUeDensity=0.000060000 # UEs
    fi
 
 #   cmd_args="src/dash/examples/lena-dash \
@@ -69,7 +62,7 @@ do
 
    cmd_args="src/dash/examples/lena-dash-ran-metrics \
 --simTime=1000 \
---randSeed=$(($i + 43)) \
+--randSeed=$(($i + 53)) \
 --numVideos=10 \
 --epc=true \
 --useUdp=false \
@@ -85,7 +78,6 @@ do
 --epcUl=true \
 --nMacroEnbSites=4 \
 "
-
 mkdir "${log_loc}/run$(($i + $1))"
 taskset -c $(($i + $2)) ./waf "$run_type" "$cmd_args" --cwd="${log_loc}/run$(($i + $1))" \
 > "${log_loc}/run$(($i + $1))/dash_client_log.txt" 2> "${log_loc}/run$(($i + $1))/mpeg_player_log.txt" &
